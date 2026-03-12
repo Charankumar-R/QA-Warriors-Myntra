b1396780252ff81f11ea545b3d097ef5dd70cce4
Karan A <karananbunidhi@gmail.com>
2026-03-12 14:16:27 +0530

    Sorting Test Case updated.

--- src/test/java/tests/ValidCouponWithSortingTest.java
+++ src/test/java/tests/ValidCouponWithSortingTest.java
@@ -27,20 +27,6 @@ public class ValidCouponWithSortingTest extends BaseTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//li[contains(@class,'product-base')]")
         ));
-
-        // ⭐ Step 3 → Apply Sorting via URL parameter
-        String url = driver.getCurrentUrl();
-
-        if (url.contains("sort=")) {
-            url = url.replaceAll("sort=[^&]*", "sort=Customer%20Rating");
-        } else if (url.contains("?")) {
-            url = url + "&sort=Customer%20Rating";
-        } else {
-            url = url + "?sort=Customer%20Rating";
-        }
-
-        driver.get(url);
-
         // ⭐ Step 4 → Wait sorted results
         wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.xpath("//li[contains(@class,'product-base')]")
@@ -48,15 +34,8 @@ public class ValidCouponWithSortingTest extends BaseTest {
 
         // ⭐ Step 5 → Select first product
         ProductsPage products = new ProductsPage(driver);
+        products.sortByCustomerRating();
         products.selectFirstProduct();
-
-        // ⭐ Step 6 → Wait until new tab opens
-//        wait.until(driver -> driver.getWindowHandles().size() > 1);
-
-        // ⭐ Step 7 → Switch window
-//
-//        for (String tab : driver.getWindowHandles()) {
-//            driver.switchTo().window(tab);
 
         // ⭐ Step 8 → Product page actions
         ProductPage product = new ProductPage(driver);
