531a005b3f0f324cfe6dea188c8f305c4fbdd807
Sree21112004 <sree21112004@gmail.com>
2026-03-12 15:04:03 +0530

    allure.properties is added

--- src/test/java/pages/BagPage.java
+++ src/test/java/pages/BagPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
 
 public class BagPage extends BasePage {
@@ -26,6 +25,18 @@ public class BagPage extends BasePage {
     @FindBy(xpath = "//span[contains(@class,'discount')]")
     WebElement priceAfter;
 
+    // coupon input field
+    @FindBy(xpath = "//input[contains(@placeholder,'coupon')]")
+    WebElement couponInputField;
+
+    // apply coupon button (for typed coupon)
+    @FindBy(xpath = "//button[contains(text(),'Apply')]")
+    WebElement applyTypedCouponButton;
+
+    // invalid coupon warning message
+    @FindBy(className = "couponsForm-base-errorMessage")
+    WebElement invalidCouponMessage;
+
     public int getPriceBeforeCoupon() {
 
         String price = priceBefore.getText().replaceAll("[^0-9]", "");
@@ -42,5 +53,39 @@ public class BagPage extends BasePage {
 
         String price = priceAfter.getText().replaceAll("[^0-9]", "");
         return Integer.parseInt(price);
+    }
+    public void enterInvalidCoupon(String coupon) {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(couponInputField));
+
+        couponInputField.clear();
+        couponInputField.sendKeys(coupon);
+    }
+
+    public void applyTypedCoupon() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(applyTypedCouponButton));
+
+        applyTypedCouponButton.click();
+    }
+
+    public String getInvalidCouponMessage() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(invalidCouponMessage));
+
+        return invalidCouponMessage.getText();
+    }
+
+    @FindBy(xpath = "//div[@data-method='couponInputApply']")
+    WebElement checkButton;
+
+    public void clickCheckButton() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(checkButton));
+        checkButton.click();
     }
 }
