06d7a353447b55ee2c1ae4724d48fc04c399b389
kailashkarthikeyan <kailashkarthikeyanms@gmail.com>
2026-03-12 14:36:57 +0530

    added DynamicQtyLimit test

--- src/test/java/pages/BagPage.java
+++ src/test/java/pages/BagPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import java.time.Duration;
 
 public class BagPage extends BasePage {
@@ -26,6 +27,18 @@ public class BagPage extends BasePage {
     @FindBy(xpath = "//span[contains(@class,'discount')]")
     WebElement priceAfter;
 
+    // Qty button (opens quantity list)
+    @FindBy(xpath = "/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[4]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]")
+    WebElement quantityButton;
+
+
+    @FindBy(xpath="//div[contains(@class,'dialogs-base-display') and text()='10']")
+    WebElement qtyTen;
+
+    // DONE button
+    @FindBy(xpath="//div[normalize-space()='DONE']")
+    WebElement doneButton;
+
     public int getPriceBeforeCoupon() {
 
         String price = priceBefore.getText().replaceAll("[^0-9]", "");
@@ -42,5 +55,21 @@ public class BagPage extends BasePage {
 
         String price = priceAfter.getText().replaceAll("[^0-9]", "");
         return Integer.parseInt(price);
+    }
+
+    public void selectMaxQuantity() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Open quantity popup
+        wait.until(ExpectedConditions.elementToBeClickable(quantityButton));
+        quantityButton.click();
+
+        // click quantity 10
+        wait.until(ExpectedConditions.elementToBeClickable(qtyTen)).click();
+
+        // click DONE
+        wait.until(ExpectedConditions.elementToBeClickable(doneButton)).click();
+
     }
 }
