9c94c5d6cd038b13a165beac72b25fe7b9abab00
Karan A <karananbunidhi@gmail.com>
2026-03-12 12:17:37 +0530

    Allure Listener and utils updated

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,17 +1,23 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setup() {
 
         driver = DriverFactory.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
         driver.get("https://www.myntra.com/");
     }
 
