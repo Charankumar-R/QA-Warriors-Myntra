4e4eadd4bef4bc6c89244a703cfdf45f99745a5c
Charankumar-R <charankumar180305@gmail.com>
2026-03-12 13:05:27 +0530

    The Valid Test Case Done with assertions.

--- src/test/java/tests/ValidCouponTest.java
+++ src/test/java/tests/ValidCouponTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import base.BaseTest;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -14,26 +15,35 @@ public class ValidCouponTest extends BaseTest {
     @Test
     @Description("Validate that a valid coupon reduces the product price")
     public void validCouponTest() {
+    HomePage homePage = new HomePage(driver);
+    homePage.searchProduct("Men T-shirt");
 
-        HomePage homePage = new HomePage(driver);
-        homePage.searchProduct("Men T-shirt");
+    ProductsPage productsPage = new ProductsPage(driver);
+    productsPage.selectFirstProduct();
 
-        ProductsPage productsPage = new ProductsPage(driver);
-        productsPage.selectFirstProduct();
+    ProductPage productPage = new ProductPage(driver);
+    productPage.selectSize();
+    productPage.addToBag();
+    productPage.openBag();
 
-        ProductPage productPage = new ProductPage(driver);
-        productPage.selectSize();
-        productPage.addToBag();
-        productPage.openBag();
+    BagPage bagPage = new BagPage(driver);
 
-        BagPage bagPage = new BagPage(driver);
+    Allure.step("Capture price before coupon");
+    int priceBefore = bagPage.getPriceBeforeCoupon();
 
-        int priceBefore = bagPage.getPriceBeforeCoupon();
+    Allure.step("Apply coupon");
+    bagPage.applyCoupon();
 
-        bagPage.applyCoupon();
+    Allure.step("Capture price after coupon");
+    int priceAfter = bagPage.getPriceAfterCoupon();
 
-        int priceAfter = bagPage.getPriceAfterCoupon();
+    System.out.println("Price before coupon: " + priceBefore);
+    System.out.println("Price after coupon: " + priceAfter);
 
-        Assert.assertTrue(priceAfter < priceBefore, "Coupon discount not applied");
+    Allure.step("Validate discount applied");
+
+    Assert.assertTrue(priceAfter < priceBefore,
+            "Coupon discount not applied. Price before: " + priceBefore + " Price after: " + priceAfter);
+
     }
 }
